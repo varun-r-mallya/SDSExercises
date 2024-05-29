0963cbe0bca83b731e2580f5ce9238f8438e7853
varun-r-mallya <varunrmallya@gmail.com>
2024-05-29 23:38:35 +0000

    initialised go project

---
+++ BACKEND/go.mod
@@ -0,0 +1,3 @@
+module github.com/varun-r-mallya/SDSExercises/tree/main/BACKEND
+
+go 1.22.3
---
+++ BACKEND/src/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+    "fmt"
+    "os"
+)
+
+
+type Page struct {
+    Title string
+    Body  []byte
+}
+
